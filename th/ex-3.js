9f534ff947a42c672dad6bf840c87e3426d8af17
CodePawsDev <isripen1991@outlook.com>
2024-10-09 09:43:42 +0700

    feat: add initial exercises in thai language

---
+++ th/ex-3.js
@@ -0,0 +1,15 @@
+/* 
+ให้เขียน Function ที่ชื่อว่า reverseText ซึ่งมีคุณสมบัติต่อไปนี้
+    - รับ Parameter 1 ตัวคือ text
+        1. text เป็น String ที่บรรจุข้อความ
+    - Function นี้จะนำ text มาสลับตัวอักษรจากหลังไปหน้า แล้ว Return ค่าออกมา
+    - เมื่อ Algorithm นี้ทำงาน จะต้องเห็นผลลัพธ์ด้านล่าง
+*/
+
+// Start coding here
+
+let text1 = "Techup";
+console.log(reverseText(text1)); // puhcet
+
+let text2 = "Hello World!!";
+console.log(reverseText(text2)); // !!dlrow olleh

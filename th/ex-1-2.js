6dd818e75cb4a29db2e80d6fcc0ac4fbafbaea27
CodePawsDev <isripen1991@outlook.com>
2024-10-16 15:12:37 +0700

    fix: changing files name

---
+++ th/ex-1-2.js
@@ -0,0 +1,29 @@
+/* 
+ให้เขียน Function ที่ชื่อว่า sumOddNumber ซึ่งมีคุณสมบัติต่อไปนี้
+    - รับ Parameter 1 ตัวคือ numbers
+        1. numbers เป็น Array ที่บรรจุตัวเลข
+    - Function นี้จะนำ numbers มาเลือกเฉพาะสมาชิกที่มีคุณสมบัติด้านล่างมาหาผลรวม  แล้ว Return ค่าออกมา
+        - เป็นเลขคี่
+        - หารด้วย 3 หรือ 5 ลงตัว
+        - หารด้วย 11 ไม่ลงตัว
+    - เมื่อ Algorithm นี้ทำงาน จะต้องเห็นผลลัพธ์ด้านล่าง
+*/
+
+// Start coding here
+
+function sumOddNumber(numbers) {
+  return numbers
+    .filter(
+      (number) =>
+        number % 2 !== 0 &&
+        (number % 3 === 0 || number % 5 === 0) &&
+        number % 11 !== 0
+    )
+    .reduce((acc, cur) => acc + cur, 0);
+}
+
+const numbers1 = [1, 2, 3, 5, 6, 8, 11, 13];
+console.log(sumOddNumber(numbers1)); // 8 จาก [3, 5]
+
+const numbers2 = [6, 8, 10, 15, 11, 19, 21, 20, 33, 55];
+console.log(sumOddNumber(numbers2)); // 36 จาก [15, 21]
--- th/ex-2.js
+++ th/ex-2.js
@@ -1,29 +1,19 @@
 /* 
-ให้เขียน Function ที่ชื่อว่า sumOddNumber ซึ่งมีคุณสมบัติต่อไปนี้
-    - รับ Parameter 1 ตัวคือ numbers
-        1. numbers เป็น Array ที่บรรจุตัวเลข
-    - Function นี้จะนำ numbers มาเลือกเฉพาะสมาชิกที่มีคุณสมบัติด้านล่างมาหาผลรวม  แล้ว Return ค่าออกมา
-        - เป็นเลขคี่
-        - หารด้วย 3 หรือ 5 ลงตัว
-        - หารด้วย 11 ไม่ลงตัว
+ให้เขียน Function ที่ชื่อว่า reverseText ซึ่งมีคุณสมบัติต่อไปนี้
+    - รับ Parameter 1 ตัวคือ text
+        1. text เป็น String ที่บรรจุข้อความ
+    - Function นี้จะนำ text มาสลับตัวอักษรจากหลังไปหน้า แล้ว Return ค่าออกมา
     - เมื่อ Algorithm นี้ทำงาน จะต้องเห็นผลลัพธ์ด้านล่าง
 */
 
 // Start coding here
 
-function sumOddNumber(numbers) {
-  return numbers
-    .filter(
-      (number) =>
-        number % 2 !== 0 &&
-        (number % 3 === 0 || number % 5 === 0) &&
-        number % 11 !== 0
-    )
-    .reduce((acc, cur) => acc + cur, 0);
+function reverseText(text) {
+  return text.toLowerCase().split("").reverse().join("");
 }
 
-const numbers1 = [1, 2, 3, 5, 6, 8, 11, 13];
-console.log(sumOddNumber(numbers1)); // 8 จาก [3, 5]
+let text1 = "Techup";
+console.log(reverseText(text1)); // puhcet
 
-const numbers2 = [6, 8, 10, 15, 11, 19, 21, 20, 33, 55];
-console.log(sumOddNumber(numbers2)); // 36 จาก [15, 21]
+let text2 = "Hello World!!";
+console.log(reverseText(text2)); // !!dlrow olleh
